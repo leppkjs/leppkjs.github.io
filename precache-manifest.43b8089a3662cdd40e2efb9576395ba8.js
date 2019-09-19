510138ae7e9682f6ce5943aa51f4819187edcc07
김재섭B <jaeseopkim@smilegate.com>
2019-09-19 15:21:14 +0900

    add rxjs

--- precache-manifest.43b8089a3662cdd40e2efb9576395ba8.js
+++ precache-manifest.43b8089a3662cdd40e2efb9576395ba8.js
@@ -1,30 +1,30 @@
 self.__precacheManifest = [
   {
-    "revision": "f7c4f54d42d16a8e3b72",
-    "url": "/js/about.c02add1d.js"
+    "revision": "f62632d7893d5bd83682",
+    "url": "/js/about.14bd6b96.js"
   },
   {
-    "revision": "25c801b6c36ab2fb1b83",
+    "revision": "68ed2898b9ff2ad447a5",
     "url": "/css/app.83a48cea.css"
   },
   {
-    "revision": "25c801b6c36ab2fb1b83",
-    "url": "/js/app.cbdf72ab.js"
+    "revision": "68ed2898b9ff2ad447a5",
+    "url": "/js/app.2b710831.js"
   },
   {
-    "revision": "b3e4b60b66fba6d2bf8c",
+    "revision": "b322422fab577f89cd9d",
     "url": "/css/chunk-vendors.37a8023c.css"
   },
   {
-    "revision": "b3e4b60b66fba6d2bf8c",
-    "url": "/js/chunk-vendors.7df04267.js"
+    "revision": "b322422fab577f89cd9d",
+    "url": "/js/chunk-vendors.93df24b7.js"
   },
   {
     "revision": "82b9c7a5a3f405032b1db71a25f67021",
     "url": "/img/logo.82b9c7a5.png"
   },
   {
-    "revision": "8069884c8990169e05caa662c27cd0f8",
+    "revision": "9f4f4e13154faeb1c5f3b0a8556297ed",
     "url": "/app.html"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0b7fdc9ac94ef43d03968b25eb9e8387.js"
+  "/precache-manifest.43b8089a3662cdd40e2efb9576395ba8.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "pwa"});
